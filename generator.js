e571f23dbb59fa818f8cc524b3a084a8443b4e96
Richard Holmes <richard.gordon.holmes@gmail.com>
2021-01-13 18:40:25 +0000

    Added test file creation and download, some styling

---
+++ generator.js
@@ -0,0 +1,120 @@
+const tab = "    "; // 4 spaces
+const quote = '"';
+
+function createTestFile(testTargetName, testClassName, testCount, testNames, testArgs, testTargets)
+{
+    let result = "";
+    result += imports;
+    result += classLine(testClassName);
+    result += "\n";
+    result += captureOutputFunction(testTargetName);
+    result += convertUnixFunction;
+    result += testFunctions(testCount, testNames, testArgs, testTargets);
+    result += "}\n";
+    return result;
+}
+
+function classLine(testClassName)
+{
+    return "public class " + testClassName + " {\n";
+}
+
+function testFunctions(testCount, testNames, testArgs, testTargets)
+{
+    let result = "";
+    for (let i = 0; i < testNames.length; i++) {
+        if (i < testCount) {
+            result += testToString(testNames[i], testArgs[i], testTargets[i]);
+        }
+    }
+    return result;
+}
+
+const imports =
+    "import static org.junit.Assert.assertEquals;\n" +
+    "\n" +
+    "import java.io.ByteArrayOutputStream;\n" +
+    "import java.io.OutputStream;\n" +
+    "import java.io.PrintStream;\n" +
+    "\n" +
+    "import org.junit.Test;" +
+    "\n";
+
+function captureOutputFunction(testTargetName)
+{
+    let result = "";
+    result +=
+        "    private String captureOutputOfMain(String args[]) {\n" +
+        "        OutputStream outputStream = new ByteArrayOutputStream();\n" +
+        "        PrintStream originalOut = System.out;\n" +
+        "        System.setOut(new PrintStream(outputStream));\n" +
+        "        try {\n";
+    result += tab + tab + tab + testTargetName + ".main(args);\n";
+    result +=
+        "        }\n" +
+        "        finally {\n" +
+        "            System.setOut(originalOut);\n" +
+        "        }\n" +
+        "        return convertToUnix(outputStream.toString().trim());\n" +
+        "    }" +
+        "\n";
+    return result;
+}
+
+const convertUnixFunction =
+    "    private String convertToUnix(String input) {\n" +
+    "        if (input == null) {\n" +
+    "            return null;\n" +
+    "        }\n" +
+    "        return input.replaceAll(\"\\r\\n\", \"\\n\").replaceAll(\"\\r\", \"\\n\");\n" +
+    "    }\n" +
+    "\n";
+
+function testToString(testName, inputArgs, targetOutput)
+{
+    let result = tab + functionHeader(testName);
+    result += tab + tab + argsToCode(inputArgs);
+    result += tab + tab + targetToCode(targetOutput);
+    result += tab + "}" + "\n";
+    result += "\n";
+    return result;
+}
+
+function functionHeader(testName)
+{
+    return "@Test\n" + tab + "public void " + testName + "() {\n";
+}
+
+function argsToCode(inputArgs)
+{
+    let result;
+    if (inputArgs.length === 0)
+    {
+        result = "String[] args = null;\n";
+    } else {
+        result = "String[] args = {";
+        for (let i = 0; i < inputArgs.length; i++) {
+            result += quote + inputArgs[i] + quote;
+            if (i < inputArgs.length - 1) {
+                result += ",";
+            }
+        }
+        result += "};\n";
+    }
+    return result;
+}
+
+function targetToCode(targetOutput)
+{
+    let result = "assertEquals(" + quote;
+    if (targetOutput.length > 0) {
+        for (let i = 0; i < targetOutput.length; i++) {
+            result += targetOutput[i];
+            if (i < targetOutput.length - 1) {
+                result += "\\n";
+            }
+        }
+    }
+    result += quote + ", captureOutputOfMain(args));\n";
+    return result;
+}
--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 const maxTests = 10;
-var testCount = 1;
 
 function generateTestForms() {
     //clones template called "divTestTemplate" maxTests times, replacing the placeholder X with the test number (i)
@@ -10,6 +9,7 @@ function generateTestForms() {
         testForm.innerHTML = testFormExample.innerHTML.replaceAll("X", i.toString());
         let testFormDiv = document.createElement("div");
         testFormDiv.id = "testForm" + i;
+        testFormDiv.className = "testForm";
         testFormDiv.innerHTML = testForm.innerHTML;
         testFormDiv.style.display = "none";
         document.getElementById("divTests").appendChild(testFormDiv);
@@ -21,13 +21,42 @@ function generateTestForms() {
 function updateTestsVisible() {
     //updates the number of testForms visible when the user changes the testCount
 
-    testCount = document.getElementById("testCount").value;
+    let testCount = document.getElementById("testCount").value;
     for (let i = 1; i <= maxTests; i++) {
         let testForm = document.getElementById("testForm" + i);
         if (i <= testCount) {
-            testForm.style.display = "inline";
+            testForm.style.display = "block";
         } else {
             testForm.style.display = "none";
         }
     }
+}
+
+// inspired by https://stackoverflow.com/a/42571423
+
+function saveFile(filename, text) {
+    let downloadBtn = document.createElement("a");
+    downloadBtn.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+    downloadBtn.setAttribute("download", filename);
+    downloadBtn.style.display = "none";
+    document.body.appendChild(downloadBtn);
+
+    downloadBtn.click();
+
+    document.body.removeChild(downloadBtn);
+}
+
+function formInputToTestFile(form) {
+    let testTargetName = form["testTargetName"].value;
+    let testClassName = form["testClassName"].value;
+    let testCount = form["testCount"].value;
+    let testNames = [], testArgs = [], testTargets = [];
+
+    for (let i = 1; i <= 10; i++) {
+        testNames[i-1] = form["testName" + i].value;
+        testArgs[i-1] = form["arguments" + i].value;
+        testTargets[i-1] = form["target" + i].value;
+    }
+
+    return (createTestFile(testTargetName, testClassName, testCount, testNames, testArgs, testTargets));
 }
