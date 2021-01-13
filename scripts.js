69993b040d6ec812f54aed8a8395b739341d6df2
Richard Holmes <richard.gordon.holmes@gmail.com>
2021-01-13 15:53:20 +0000

    Test forms visible updates with test count

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 const maxTests = 10;
+var testCount = 1;
 
 function generateTestForms() {
-    //clones template called "divTestX" maxTests times, replacing the placeholder X with the test number (i)
-    let testFormExample = document.getElementById("divTestX");
+    //clones template called "divTestTemplate" maxTests times, replacing the placeholder X with the test number (i)
+    let testFormExample = document.getElementById("divTestTemplate");
 
     for (let i = 1; i <= maxTests; i++) {
         let testForm = testFormExample.content.cloneNode(true);
@@ -10,6 +11,23 @@ function generateTestForms() {
         let testFormDiv = document.createElement("div");
         testFormDiv.id = "testForm" + i;
         testFormDiv.innerHTML = testForm.innerHTML;
+        testFormDiv.style.display = "none";
         document.getElementById("divTests").appendChild(testFormDiv);
+    }
+
+    updateTestsVisible();
+}
+
+function updateTestsVisible() {
+    //updates the number of testForms visible when the user changes the testCount
+
+    testCount = document.getElementById("testCount").value;
+    for (let i = 1; i <= maxTests; i++) {
+        let testForm = document.getElementById("testForm" + i);
+        if (i <= testCount) {
+            testForm.style.display = "inline";
+        } else {
+            testForm.style.display = "none";
+        }
     }
 }
